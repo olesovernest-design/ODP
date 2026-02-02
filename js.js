6af1a4c9d5568e091ba020de18d27085baf79a14
olesovernest-design <olesovernest@gmail.com>
2026-02-02 09:37:14 +0900

    Add files via upload

---
+++ js.js
@@ -0,0 +1,51 @@
+function red() {
+    document.getElementById("block").style.backgroundColor = "red"
+}
+
+function green(){
+    document.getElementById("block").style.backgroundColor = "green"
+}
+
+function blue(){
+    document.getElementById("block").style.backgroundColor = "blue"
+}
+
+function yellow(){
+    document.getElementById("block").style.backgroundColor = "yellow"
+}
+
+function orange(){
+    document.getElementById("block").style.backgroundColor = "orange"
+}
+
+function skyblue(){
+    document.getElementById("block").style.backgroundColor = "skyblue"
+}
+
+function black(){
+    document.getElementById("block").style.backgroundColor = "black"
+}
+
+function brown(){
+    document.getElementById("block").style.backgroundColor = "brown"
+}
+
+function white(){
+    document.getElementById("block").style.backgroundColor = "white"
+}
+
+function purple(){
+    document.getElementById("block").style.backgroundColor = "purple"
+}
+
+function darkgreen(){
+    document.getElementById("block").style.backgroundColor = "darkgreen"
+}
+
+function darkred(){
+    document.getElementById("block").style.backgroundColor = "darkred"
+}
+
+function Clear(){
+    document.getElementById("block").style.backgroundColor = "Clear"
+}
